d12652dd2226e6801269ce116b9835d4d0bb6754
Stanislav <stanislav.pogorskii@gmail.com>
2024-02-01 18:51:52 +0400

    added root directory handler

--- main.go
+++ main.go
@@ -1,14 +1,21 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	gin.SetMode(gin.ReleaseMode)
 	router := gin.Default()
+	router.GET("/", handleRootRequest)
 	router.GET("/games/update", handleGamesUpdateRequest)
 	router.GET("/movies/update", handleMoviesUpdateRequest)
 	router.GET("/tvshows/update", handleTVShowsUpdateRequest)
 	router.Run("localhost:8080")
+}
+
+func handleRootRequest(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, "Hello, the API works")
 }
